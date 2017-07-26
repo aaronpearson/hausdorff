a3d63b7a757fd9895409e87305f6699d8853d04f
Aaron Pearson <apearson@lakeheadu.ca>
2017-07-26 12:47:28 -0400

    Fixed indexing problem in top method

--- series.py
+++ series.py
@@ -5,9 +5,12 @@ class RecSeries():
 		resultlist=[self.origin]
 		for x in range(1,n):
 			formindex = str(x-1)
-			form= "y = resultlist[" + formindex + "]" + self.formulastring
-			eval(form)
-			resultlist[n] = y
-
+			#form= "y = resultlist[" + formindex + "]" + self.formulastring
+			y = eval('resultlist[' + formindex + "]" + self.formulastring)	
+			resultlist.append(y) #fixed index error
+		return resultlist
 			
 		
+testseries=RecSeries()
+testseries.origin=1
+testseries.formulastring="+1"
